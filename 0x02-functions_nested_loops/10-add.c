10dda0f398e1acef9ccd0d5c9a0bfd191a347f95
adaemeka <ada4emeka@yahoo.com>
2023-05-24 05:35:05 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds two integers
+ * @n: number one.
+ * @m: number two.
+ * Return: Add of number one and number two.
+ */
+int add(int n, int m)
+{
+return (n + m);
+}
+
