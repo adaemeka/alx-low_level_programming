223b7b157faff18fd239041b86e749e9b1f20c0b
adaemeka <ada4emeka@yahoo.com>
2023-05-27 05:26:45 +0100

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: First integer
+ * @b: Second integer.
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
