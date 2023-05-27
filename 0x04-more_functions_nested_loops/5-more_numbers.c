210482319aa8f82ed332fe8ffd522c3ebcc5e7a4
adaemeka <ada4emeka@yahoo.com>
2023-05-27 06:10:32 +0100

    constitute

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 (10 times).
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+int i, a;
+
+for (i = 0; i < 10; i++)
+{
+for (a = 0; a < 15; a++)
+{
+if (a >= 10)
+_putchar((a / 10) + 48);
+_putchar((a % 10) + 48);
+}
+_putchar('\n');
+}
+}
+
